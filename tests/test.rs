b0775ab1c3b7a5b23adb0ddcb45138f9992e20ca
elftausend <skatsacc.gut24@gmail.com>
2022-04-12 22:42:03 +0200

    added tests

--- tests/test.rs
+++ tests/test.rs
@@ -6,3 +6,41 @@ fn test_plot() {
     let plot = Plot::new(([-4., -2., 1., 4.], "o"));
     plot.show();
 }
+
+#[test]
+fn test_sine_wave() {
+    let mut xs = [0.; 1000]; 
+    
+    let mut add = 0f64;
+    for idx in 0..1000 {
+        xs[idx] = add/1000.;
+        add += 1.;
+    }
+     
+    let mut ys = [0.; 1000];
+    for (i, y) in ys.iter_mut().enumerate() {
+        *y = (2. * std::f64::consts::PI * xs[i]).sin();
+    }
+
+    let plot = Plot::new((xs, ys));
+    plot.show();
+}
+
+#[test]
+fn test_x_squared() {
+    let mut xs = [0.; 10000]; 
+    
+    let mut add = -5000f64;
+    for idx in 0..10000 {
+        xs[idx] = add/1000.;
+        add += 1.;
+    }
+     
+    let mut ys = [0.; 10000];
+    for (i, y) in ys.iter_mut().enumerate() {
+        *y = xs[i].powf(2.);
+    }
+
+    let plot = Plot::new((xs, ys));
+    plot.show();
+}
