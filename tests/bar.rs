20c7eb1567b995ccdbd04dd07afc8f8dfd717ce8
elftausend <skatsacc.gut24@gmail.com>
2022-06-23 22:09:32 +0200

    negative bar values

--- tests/bar.rs
+++ tests/bar.rs
@@ -8,3 +8,20 @@ fn test_basic_bar() {
     bar.set_ylabel("test");
     bar.show();
 }
+#[test]
+fn test_neg_basic_bar() {
+    let mut bar = Bar::new([("Ferris", RED), ("Stefan", GREEN), ("Test", BLUE)], &[100., 200., 500.,]);
+    bar.add(("Added", ORANGE), -400.);
+    bar.set_title("title");
+    bar.set_ylabel("test");
+    bar.show();
+}
+
+#[test]
+fn test_small_bar() {
+    let mut bar = Bar::new([("Ferris", RED), ("Stefan", GREEN), ("Test", BLUE)], &[0.1, 0.2, 0.5,]);
+    bar.add(("Added", ORANGE), -0.4);
+    bar.set_title("title");
+    bar.set_ylabel("test");
+    bar.show();
+}
